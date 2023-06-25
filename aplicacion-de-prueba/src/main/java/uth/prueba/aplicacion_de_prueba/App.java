b5344d8ebf7c33fbc2998d086f0bd199e094c462
DESKTOP-LF8IAPM\DELL <wencellrivera78@gmnail.com>
2023-06-25 00:01:17 -0600

    Prueba
    Agregando contenido a repositorio

---
+++ aplicacion-de-prueba/src/main/java/uth/prueba/aplicacion_de_prueba/App.java
@@ -0,0 +1,14 @@
+package uth.prueba.aplicacion_de_prueba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String nombre = "Wencell Rivera";
+        System.out.println( "!Bienvenido a nuestro sistema!" +nombre) ;
+    }
+}
